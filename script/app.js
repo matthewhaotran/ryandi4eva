9ddaed52a48eb0ce410dffe90518023f0d5b5a6e
Matthew Tran <matthew@Matthews-MacBook-Pro.local>
2019-07-08 15:38:22 -0700

    added 20th rule

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 let miniGames = {
-    1: 'You win!  Take a drink yourself',
-    2: 'Choose someone to drink!',
+    1: 'Me - Take a drink yourself!',
+    2: 'You - Choose someone to drink!',
     3: 'All girls drink!',
     4: 'All guys drink!',
     5: 'Drive: Vroom or Erk',
@@ -11,13 +11,20 @@ let miniGames = {
     10: 'Categories: Say things in a category until someone messes up',
     11: '3/6/9 - Count and clap on 3/6/9 until somone messes up',
     12: 'Baskin Robbins - Whoever lands on 31 drinks!',
-    13: "I'm leaving",
+    13: "I'm leaving - Last one left loses!",
+    14: "Waterfall - Choose 2 others and play waterfall",
+    15: "Paper Rock Scissors - Play 1v1 against everyone, loser of each match drinks",
+    16: "Never have I ever - elimination",
+    17: "One time thumbmaster",
+    18: "Vikings row!",
+    19: "Nose Goes - Last person to touch the nose drinks!",
+    20: "All singles drink!"
 }
 
 $('#card').click(
     function () {
         min = Math.ceil(1);
-        max = Math.floor(13);
+        max = Math.floor(20);
         var game = Math.floor(Math.random() * (max - min + 1)) + min;
         $('#cardText').text(miniGames[game]);
     }
